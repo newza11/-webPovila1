46ef4af20c10414238b9503b049db6a00186c377
newza11 <kulwadee45@gmail.com>
2024-09-13 17:04:09 +0700

    newgen1

--- database/my_website (2).sql
+++ database/my_website (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 27, 2024 at 12:23 PM
+-- Generation Time: Sep 13, 2024 at 12:03 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -39,7 +39,7 @@ CREATE TABLE `accordion_items` (
 --
 
 INSERT INTO `accordion_items` (`id`, `title`, `description`, `image_path`) VALUES
-(7, 'สระว่ายน้ำ', 'ให้คุณว่ายน้ำ หรือเล่นสไลด์เดอร์เพื่อความสนุกสนาน เล่นน้ำหย่อนใจอย่างสมบูรณ์แบบและนั่งเล่นริมสระ มอบความเป็นส่วนตัวในการพักผ่อน มีสระสำหรับเด็กๆเล่นอย่างปลอดภัยมีชูชิพ และน้องเป็ดเหลืองเล่นน้ำเป็นเพื่อนอีกด้วย', 'uploads/2019-01-29_06-14-27_406714-400x300dd.jpg'),
+(7, 'สระว่ายน้ำ', 'ให้คุณว่ายน้ำ หรือเล่นสไลด์เดอร์เพื่อความสนุกสนาน เล่นน้ำหย่อนใจอย่างสมบูรณ์แบบและนั่งเล่นริมสระ มอบความเป็นส่วนตัวในการพักผ่อน มีสระสำหรับเด็กๆเล่นอย่างปลอดภัยมีชูชีพ และน้องเป็ดเหลืองเล่นน้ำเป็นเพื่อนอีกด้วย', 'uploads/2019-01-29_06-14-27_406714-400x300dd.jpg'),
 (8, 'ห้องคาราโอเกะ', 'มีห้องคาราโอเกะ พร้อมโต๊ะสนุ๊กให้บริการสำหรับผู้ที่ชื่นชอบเสียงเพลง พร้อมทั้งมีไฟเทค เสริมความสนุกสนานของร้องเพลง ภายในห้องดีไซน์แบบ Luxury มีความหรูหราอย่างลงตัว', 'uploads/home11.jpg'),
 (9, 'ห้องครัว', 'ห้องครัวอุปกรณ์ครัวครบครันใกล้สระว่ายน้ำ มีทั้งครัวไทยละครัวฝรั่ง สามารถทำอาหารได้หลากหลายอย่างละ มีเตาย่างบาร์บีคิวโต๊ะกินข้าวบริเวณริมสระน้ำ', 'uploads/ddddddddddddddddddddddd.jpg');
 
@@ -63,10 +63,39 @@ CREATE TABLE `contact` (
 --
 
 INSERT INTO `contact` (`id`, `name`, `email`, `phone`, `message`, `created_at`) VALUES
-(14, 'ปัญจพล เพชรมณีนิลใส', '11111@gmail.com', '0613989655', '11', '2024-08-21 07:19:35'),
 (15, 'ปัญจพล เพชรมณีนิลใส', 'kulwadee45@gmail.com', '0613989655', '11', '2024-08-21 07:22:24'),
 (17, 'ปัญจพล เพชรมณีนิลใส', 'kulwadee45@gmail.com', '0613989655', 'ๅๅๅๅๅๅๅๅๅๅๅๅๅๅๅๅๅๅๅๅ', '2024-08-21 07:26:12'),
-(18, 'ปัญจพล เพชรมณีนิลใส', 'kulwadee45@gmail.com', '0613989655', '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111', '2024-08-21 07:26:27');
+(23, 'ปัญจพล', 'kulwadee45@gmail.com', '0613989655', 'ๅ', '2024-09-13 09:56:41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `holidays_db`
+--
+
+CREATE TABLE `holidays_db` (
+  `id` int(11) NOT NULL,
+  `holiday_name` varchar(255) NOT NULL,
+  `holiday_date` date NOT NULL,
+  `holiday_price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `holidays_db`
+--
+
+INSERT INTO `holidays_db` (`id`, `holiday_name`, `holiday_date`, `holiday_price`) VALUES
+(2, 'Makha Bucha Day', '2024-02-26', 14900.00),
+(3, 'Songkran', '2024-04-13', 16900.00),
+(4, 'Songkran', '2024-04-14', 16900.00),
+(5, 'Songkran', '2024-04-15', 16900.00),
+(6, 'Labour Day', '2024-05-01', 14900.00),
+(7, 'HM King\'s Birthday', '2024-07-28', 14900.00),
+(8, 'Queen Mother\'s Birthday', '2024-08-12', 14900.00),
+(9, 'Father\'s Day', '2024-12-05', 14900.00),
+(10, 'Constitution Day', '2024-12-10', 14900.00),
+(11, 'New Year\'s Eve', '2024-12-31', 16900.00),
+(12, 'New Year\'s Day', '2025-01-01', 16900.00);
 
 -- --------------------------------------------------------
 
@@ -88,16 +117,14 @@ CREATE TABLE `login_user` (
 --
 
 INSERT INTO `login_user` (`id`, `email`, `name`, `password`, `role`, `profile_picture`) VALUES
-(15, 'kulwadee45@gmail.com', '1', '$2y$10$tDyF.om0BPSibSne4oLke.Dk9y.ZmrxxvFKvSIrZxW57rJYDyojrG', 'User', 'uploads/cat.webp'),
+(15, 'kulwadee45@gmail.com', 'new', '$2y$10$tDyF.om0BPSibSne4oLke.Dk9y.ZmrxxvFKvSIrZxW57rJYDyojrG', 'User', 'uploads/317725595_619719976569850_914231069959707125_n.jpg'),
 (16, '1@gmail.com', '12', '$2y$10$kKdMHcKmG2osJBfFyjJUqe1qoIp/bIBbURVx3jBH3RxFr.oATcWDu', 'User', 'uploads/317725595_619719976569850_914231069959707125_n.jpg'),
-(68, '3@gmail.com', 'new', '$2y$10$hxtkOkpoUTh2PxJjed7SPebkJqnjjbaAPsa6fnuxTNNR7dyG3uv/.', 'User', ''),
-(69, 'admin.@gmail.com', '12', '$2y$10$lMeKPKxmwww6ZZ.CuqcxVOgY9jhiZtbFneAwO/liq3XpZVH07ADp2', 'admin', 'uploads/356182793_783017686943161_5396556727909400559_n.jpg'),
-(78, 'admin@gmail.com', 'admin', '$2y$10$D9UEN.OEwcj2CbtfhhflleFiSORrnSvsnSBATL8ir.01fYO1p1Uxa', 'Admin', ''),
+(69, 'admin.@gmail.com', 'superadmin', '$2y$10$HMlobgQTvEjvC3guHD72HeV2XXQxrbOPOJqDLPVcQn9BsTZSjWmcG', 'Admin', 'uploads/356182793_783017686943161_5396556727909400559_n.jpg'),
+(78, 'admin@gmail.com', 'admin', '$2y$10$6TQembZxTZ8HvRIINcg5y.BHkS9ubFUEZ/n3u8gdePJKpnSKOUa5O', 'Admin', 'uploads/NEWHOME.jpg'),
 (83, '11@gmail.com', 'new', '$2y$10$4vARCiiyDZUlabP2XwfVAeHRRYaVeEJJ94PCJuswTTDKfIendFXly', 'user', ''),
 (84, 'kulwadedde45@gmail.com', 'กกกก', '$2y$10$scg1NMq5wHLRznS99xFZ8es2Q/wofYPBDU6tZ5HvkptFnlwhtmJSW', 'User', ''),
-(86, 'chanlika.naa@gmail.com', 'chanlika', '$2y$10$3dPyjVwy/eS9TN5u/OKOFexivPE9WQ0BJLrOPrFxCJfA3zN0i1lPK', 'user', 'uploads/317725595_619719976569850_914231069959707125_n.jpg'),
-(90, 'kulwadee4115@gmail.com', 'new', '$2y$10$mxxHnLgry39McmLsI5Uk2.7BBfwUOjhBjBu9NSlXUXIMcgSDSWSDe', 'user', ''),
-(91, 'ad@ad.com', 'new', '$2y$10$KPFpHAuLl6gu19VjQ69CQOrL6yDPWWwf1OxdRN1rpcg.T8CBWwwKq', 'admin', '');
+(86, 'chanlika.naa@gmail.com', 'chanlika', '$2y$10$YfQlIe51RB0b.il/NCbzO.v8Oy/ZvmgdoA2cuT4Omddo3dTOcSpYS', 'User', 'uploads/317725595_619719976569850_914231069959707125_n.jpg'),
+(91, 'ad@ad.com', 'new', '$2y$10$KPFpHAuLl6gu19VjQ69CQOrL6yDPWWwf1OxdRN1rpcg.T8CBWwwKq', 'Admin', 'uploads/IMG_2337.jpeg');
 
 -- --------------------------------------------------------
 
@@ -127,28 +154,21 @@ CREATE TABLE `orders_db` (
 
 INSERT INTO `orders_db` (`id`, `name`, `price`, `people`, `checkin`, `checkout`, `status`, `phone`, `room`, `firstname`, `lastname`, `slip`, `user_id`) VALUES
 (61, 'ปัญจพล เพชรมณีนิลใส', 7900, 12, '2024-08-22', '2024-08-23', 'Waiting to enter', '0613989655', '4ห้อง', 'ปัญจพล', 'เพชรมณีนิลใส', 'uploads/317725595_619719976569850_914231069959707125_n.jpg', '16'),
-(101, 'ปัญจพล เพชรมณีนิลใส', 8900, 4, '2024-08-26', '2024-08-27', 'check', '0613989655', '5ห้อง', 'ปัญจพล', 'เพชรมณีนิลใส', 'uploads/317725595_619719976569850_914231069959707125_n.jpg', '69'),
-(111, 'ปัญจพล เพชรมณีนิลใส', 8900, 4, '2024-08-26', '2024-08-27', 'check', '0613989655', '5ห้อง', 'ปัญจพล', 'เพชรมณีนิลใส', 'uploads/317725595_619719976569850_914231069959707125_n.jpg', '69'),
-(112, 'ปัญจพล เพชรมณีนิลใส', 8900, 6, '2024-08-28', '2024-08-29', 'check', '0613989655', '5ห้อง', 'ปัญจพล', 'เพชรมณีนิลใส', 'uploads/BakeLab+Food+Photography-1001.jpg', '16'),
-(126, 'ปัญจพล เพชรมณีนิลใส', 8900, 6, '2024-08-28', '2024-08-29', 'check', '0613989655', '5ห้อง', 'ปัญจพล', 'เพชรมณีนิลใส', 'uploads/pay.jpg', '16'),
-(127, 'ไม่ระบุ', 0, 0, '0000-00-00', '0000-00-00', 'check', 'ไม่ระบุ', 'ไม่ระบุ', 'ไม่ระบุ', 'ไม่ระบุ', 'uploads/pay.jpg', '69'),
-(128, 'ไม่ระบุ', 0, 0, '0000-00-00', '0000-00-00', 'check', 'ไม่ระบุ', 'ไม่ระบุ', 'ไม่ระบุ', 'ไม่ระบุ', 'uploads/pay.jpg', '69'),
-(129, 'panchapol pechmaneeninsai', 8900, 2, '2024-08-29', '2024-08-30', 'check', '0613989655', '5ห้อง', 'panchapol', 'pechmaneeninsai', 'uploads/pay.jpg', '69'),
-(130, 'panchapol pechmaneeninsai', 8900, 2, '2024-08-29', '2024-08-30', 'check', '0613989655', '5ห้อง', 'panchapol', 'pechmaneeninsai', 'uploads/pay.jpg', '69'),
-(131, 'panchapol pechmaneeninsai', 8900, 2, '2024-08-29', '2024-08-30', 'check', '0613989655', '5ห้อง', 'panchapol', 'pechmaneeninsai', 'uploads/pay.jpg', '69'),
-(132, 'panchapol pechmaneeninsai', 8900, 2, '2024-08-29', '2024-08-30', 'check', '0613989655', '5ห้อง', 'panchapol', 'pechmaneeninsai', 'uploads/pay.jpg', '69'),
+(130, 'panchapol pechmaneeninsai', 8900, 2, '2024-08-29', '2024-08-30', 'Completed', '0613989655', '5ห้อง', 'panchapol', 'pechmaneeninsai', 'uploads/pay.jpg', '69'),
+(131, 'panchapol pechmaneeninsai', 8900, 2, '2024-08-29', '2024-08-30', 'Cancel', '0613989655', '5ห้อง', 'panchapol', 'pechmaneeninsai', 'uploads/pay.jpg', '69'),
+(132, 'panchapol pechmaneeninsai', 8900, 2, '2024-08-29', '2024-08-30', 'Waiting to enter', '0613989655', '5ห้อง', 'panchapol', 'pechmaneeninsai', 'uploads/pay.jpg', '69'),
 (133, 'panchapol pechmaneeninsai', 8900, 2, '2024-08-29', '2024-08-30', 'check', '0613989655', '5ห้อง', 'panchapol', 'pechmaneeninsai', 'uploads/Huawei-Logo.jpg', '69'),
 (134, 'panchapol pechmaneeninsai', 8900, 2, '2024-08-29', '2024-08-30', 'check', '0613989655', '5ห้อง', 'panchapol', 'pechmaneeninsai', 'uploads/Huawei-Logo.jpg', '69'),
-(135, 'panchapol pechmaneeninsai', 8900, 2, '2024-08-29', '2024-08-30', 'check', '0613989655', '5ห้อง', 'panchapol', 'pechmaneeninsai', 'uploads/Huawei-Logo.jpg', '69'),
-(136, 'panchapol pechmaneeninsai', 8900, 2, '2024-08-29', '2024-08-30', 'check', '0613989655', '5ห้อง', 'panchapol', 'pechmaneeninsai', 'uploads/Huawei-Logo.jpg', '69'),
-(137, 'ปัญจพล เพชรมณีนิลใส', 12900, 11, '2024-08-30', '2024-08-31', 'check', '0613989655', '6ห้อง', 'ปัญจพล', 'เพชรมณีนิลใส', 'uploads/pay.jpg', '69'),
-(138, 'ปัญจพล เพชรมณีนิลใส', 12900, 11, '2024-08-30', '2024-08-31', 'check', '0613989655', '6ห้อง', 'ปัญจพล', 'เพชรมณีนิลใส', 'uploads/pay.jpg', '69'),
-(139, 'ปัญจพล เพชรมณีนิลใส', 12900, 11, '2024-08-30', '2024-08-31', 'check', '0613989655', '6ห้อง', 'ปัญจพล', 'เพชรมณีนิลใส', 'uploads/pay.jpg', '69'),
-(140, 'ปัญจพล เพชรมณีนิลใส', 8900, 12, '2024-08-27', '2024-08-28', 'check', '0613989655', '5ห้อง', 'ปัญจพล', 'เพชรมณีนิลใส', 'uploads/Huawei-Logo.jpg', '16'),
-(141, 'ปัญจพล เพชรมณีนิลใส', 8900, 4, '2024-09-04', '2024-09-05', 'Waiting to enter', '0613989655', '5ห้อง', 'ปัญจพล', 'เพชรมณีนิลใส', 'uploads/pay.jpg', '16'),
-(142, 'new', 14900, 1, '2024-08-31', '2024-09-01', 'check', '1', '6ห้อง', 'mew', 'new', 'uploads/IMG_4399.png', '15'),
-(143, 'ปัญจพล เพชรมณีนิลใส', 9900, 6, '2024-09-19', '2024-09-20', 'check', '0613989655', '6ห้อง', 'ปัญจพล', 'เพชรมณีนิลใส', 'uploads/d3456.jpg', '15'),
-(144, 'ปัญจพล เพชรมณีนิลใส', 9900, 6, '2024-09-19', '2024-09-20', 'check', '0613989655', '6ห้อง', 'ปัญจพล', 'เพชรมณีนิลใส', 'uploads/d3456.jpg', '15');
+(135, 'panchapol pechmaneeninsai', 8900, 2, '2024-08-29', '2024-08-30', 'Completed', '0613989655', '5ห้อง', 'panchapol', 'pechmaneeninsai', 'uploads/Huawei-Logo.jpg', '69'),
+(150, 'ryr35435435 67565465454 ', 26700, 20, '2024-09-23', '2024-09-26', 'check', '4', '5ห้อง', '4', '4', 'uploads/image (2).png', '15'),
+(151, 'ปัญจพล เพชรมณีนิลใส', 35600, 1, '2024-09-26', '2024-09-30', 'check', '0613989655', '3ห้อง', 'ปัญจพล', 'เพชรมณีนิลใส', 'uploads/pay.jpg', '15'),
+(152, 'นิว', 9900, 3, '2024-09-04', '2024-09-05', 'check', '0613989655', '6ห้อง', 'ปัญจพล', 'เพชรมณีนิลใส', 'uploads/pay.jpg', '78'),
+(154, 'new', 8900, 4, '2024-09-18', '2024-09-19', 'check', '0613989655', '5ห้อง', 'panchapol', 'pechmaneeninsai', 'uploads/458240151_3888955781393788_7086781230857662308_n.jpg', '78'),
+(156, 'ปัญจพล', 17800, 3, '2024-10-02', '2024-10-04', 'check', '0613989655', '5ห้อง', 'ปัญจพล', 'เพชรมณีนิลใส', 'uploads/458240151_3888955781393788_7086781230857662308_n.jpg', '15'),
+(158, 'ปัญจพล', 7900, 2, '2024-09-19', '2024-09-20', 'check', '0613989655', '4ห้อง', 'ปัญจพล', 'เพชรมณีนิลใส', 'uploads/NEWHOME.jpg', '78'),
+(160, 'ปัญจพล', 0, 0, '0000-00-00', '0000-00-00', 'check', '0613989655', 'ไม่ระบุ', 'ปัญจพล', 'เพชรมณีนิลใส', 'uploads/pay.jpg', '78'),
+(161, 'ปัญจพล', 16900, 5, '2025-01-01', '2025-01-02', 'check', '0613989655', '6ห้อง', 'ปัญจพล', 'เพชรมณีนิลใส', 'uploads/pay.jpg', '78'),
+(162, 'ปัญจพล', 8900, 4, '2025-01-15', '2025-01-16', 'check', '0613989655', '5ห้อง', 'ปัญจพล', 'เพชรมณีนิลใส', 'uploads/pay.jpg', '78');
 
 -- --------------------------------------------------------
 
@@ -191,9 +211,9 @@ INSERT INTO `villa_about` (`content`, `id`) VALUES
 ('พูลวิลล่าอัมพวา', 1),
 ('จองพูลวิลล่าอัมพวากับเรา (Pool Villa Amphawa) ที่พักหรูหรา และบ้านพักส่วนตัวที่ตอบโจทย์ความต้องการของคุณ ไม่ว่าจะเป็นการพักผ่อนอย่างสงบในบรรยากาศที่เป็นธรรมชาติ และเต็มไปด้วยความเป็นส่วนตัว สัมผัสประสบการณ์การพักผ่อนที่แตกต่างกับบริการที่เหนือชั้น', 2),
 ('เหตุผลที่ควรเลือกใช้บริการกับเรา', 3),
-('คุ้มค่า ราคายุติธรรม', 4),
+('คุ้มค่า ราคาดี', 4),
 ('สะดวกสบาย ติดต่อได้ตลอด 24 ชั่วโมง', 5),
-('ดูแลด้วยใจ', 6);
+('ดูแลด้วยใจและใส่ใจ', 6);
 
 -- --------------------------------------------------------
 
@@ -236,7 +256,7 @@ CREATE TABLE `villa_details` (
 --
 
 INSERT INTO `villa_details` (`id`, `detail_type`, `detail_description`) VALUES
-(1, 'ราคาเหมาทั้งหลัง (6 ห้องนอน)', 'วันอาทิตย์ - วันพฤหัส ราคาเหมา 9,900.- บาท/คืน\r\n                      <br />วันศุกร์ ราคาเหมา 12,900.- บาท/คืน<br />วันเสาร์/วันหยุดนขัตฤกษ์\r\n                      ราคาเหมา 14,900.- บาท/คืน <br />วันขึ้นปีใหม่ วันสงกรานต์ ราคาเหมา 16,900.-                      บาท/คืน'),
+(1, 'ราคาเหมาทั้งหลัง (6 ห้องนอน)', 'วันอาทิตย์ - วันพฤหัส ราคาเหมา 9,900.- บาท/คืน                      <br />วันศุกร์ ราคาเหมา 12,900.- บาท/คืน<br />วันเสาร์/วันหยุดนขัตฤกษ์                      ราคาเหมา 14,900.- บาท/คืน <br />วันขึ้นปีใหม่ วันสงกรานต์ ราคาเหมา 16,900.-                      บาท/คืน'),
 (2, 'รายละเอียดแต่ละห้อง', '- ห้องนอนที่ 1 ติดสระว่ายน้ำ ประกอบด้วย เตียงนอนขนาด 5 ฟุต <br /> 2 เตียง ห้องน้ำในตัว TV และโต๊ะเครื่องแป้ง<br />\n                      - ห้องนอนที่ 2 ประกอบด้วย เตียงนอนขนาด 5 ฟุต 2 เตียง <br/> ห้องน้ำในตัวพร้อม TV และโต๊ะเครื่องแป้ง<br />\n                      - ห้องนอนที่ 3 ห้องนอนเด็ก ประกอบด้วย เตียงนอนขนาด 5 ฟุต 2 เตียง <br />\n                      - ห้องนอนที่ 4 ประกอบด้วย เตียงนอนขนาด 5 ฟุต 2 เตียง TV และโต๊ะเครื่องแป้ง<br />\n                      - ห้องนอนที่ 5 ประกอบด้วย เตียงนอนขนาด 5 ฟุต 2 เตียง<br />\n                      - ห้องนอนที่ 6 ประกอบด้วย เตียงนอนขนาด 6 ฟุต 1 เตียง <br /> ห้องน้ำในตัว\n                        พร้อมอ่างจากุชชี่ TV และโต๊ะเครื่องแป้ง'),
 (3, 'ราคาวันธรรมดา (วันอาทิตย์ - วันพฤหัส)', '3 ห้อง ราคาเหมา 6,900.- บาท/คืน<br />\r\n                      4 ห้อง ราคาเหมา 7,900.- บาท/คืน<br />\r\n                      5 ห้อง ราคาเหมา 8,900.- บาท/คืน'),
 (4, 'ลานจอดรถ', 'จอดได้ 3 คัน'),
@@ -261,7 +281,7 @@ CREATE TABLE `villa_home_content` (
 --
 
 INSERT INTO `villa_home_content` (`id`, `title`, `description`, `image_path`) VALUES
-(2, 'บ้านนันท์นภัส พลูวิลล่า อัมพวา', ' Luxury Pool Villa <br />บ้านพัก 6 ห้องนอน 6 ห้องน้ำ <br />รองรับลูกค้าถึง 20 ท่าน/คืน<br />ตกแต่งสไตล์              Luxury มีความหรูหรา <br />มาพร้อมห้องคาราโอเกะที่กว้างขวาง              <br /> มีสระว่ายน้ำ สไลด์เดอร์               อุปกรณ์เครื่องครัวครบครัน ใกล้สถานที่ท่องเที่ยวมากมาย <br />              พร้อมให้บริการลูกค้าทุกท่าน', 'poo\\home1.jpg'),
+(2, 'บ้านนันท์นภัส พลูวิลล่า อัมพวา', ' Luxury Pool Villa <br />บ้านพัก 6 ห้องนอน 6 ห้องน้ำ <br />รองรับลูกค้าถึง 20 ท่าน/คืน<br />ตกแต่งสไตล์              Luxury มีความหรูหรา <br />มาพร้อมห้องคาราโอเกะที่กว้างขวาง              <br /> มีสระว่ายน้ำ สไลด์เดอร์               อุปกรณ์เครื่องครัวครบครัน ใกล้สถานที่ท่องเที่ยวมากมาย <br />              พร้อมให้บริการลูกค้าทุกท่าน', 'uploads/home1.jpg'),
 (4, '', '', 'poo\\home4.jpg'),
 (5, '', '', 'poo/home3.jpg'),
 (6, '', '', 'poo/home9.jpg'),
@@ -307,7 +327,7 @@ INSERT INTO `villa_images` (`id`, `image_path`, `too`) VALUES
 (20, 'poo/20.jpg', ''),
 (21, 'poo/21.jpg', ''),
 (22, 'poo/22.jpg', ''),
-(23, 'poo/23.jpg', '');
+(23, 'uploads/home11.jpg', '');
 
 -- --------------------------------------------------------
 
@@ -328,7 +348,7 @@ CREATE TABLE `villa_main` (
 INSERT INTO `villa_main` (`id`, `content`, `image_path`) VALUES
 (1, 'บ้านนันท์นภัส', 'uploads/home2.jpg'),
 (2, 'พลูวิลล่า อัมพวา', 'poo/10.jpg'),
-(3, 'Luxury Pool Villa บ้านพัก 6 ห้องนอน 6 ห้องน้ำที่ตกแต่งไปด้วยสไตล์ Luxury มีความหรูหรา<br>มาพร้อมห้องคาราโอเกะที่กว้างขวาง พร้อมรองรับลูกค้าถึง 20 ท่าน มาพร้อมกับสระว่ายน้ำ สไลด์เดอร์<br> อุปกรณ์ครัวครบครันใกล้สถานที่ท่องเที่ยวมากมาย ณ อัมพวา พร้อมให้บริกา', 'poo/3.jpg'),
+(3, 'Luxury Pool Villa บ้านพัก 6 ห้องนอน 6 ห้องน้ำที่ตกแต่งไปด้วยสไตล์Luxuryมีความหรูหรา<br>มาพร้อมห้องคาราโอเกะที่กว้างขวาง พร้อมรองรับลูกค้าถึง 20 ท่าน มาพร้อมกับสระว่ายน้ำ สไลด์เดอร์<br> อุปกรณ์ครัวครบครันใกล้สถานที่ท่องเที่ยวมากมาย ณ อัมพวา พร้อมให้บริการ', 'poo/3.jpg'),
 (7, 'นันท์นภัส พลูวิลล่า อัมพวา', 'poo/4.jpg'),
 (8, '', 'poo/5.jpg'),
 (9, '', 'poo/2.jpg'),
@@ -348,6 +368,12 @@ ALTER TABLE `accordion_items`
 -- Indexes for table `contact`
 --
 ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `holidays_db`
+--
+ALTER TABLE `holidays_db`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -418,25 +444,31 @@ ALTER TABLE `accordion_items`
 -- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `holidays_db`
+--
+ALTER TABLE `holidays_db`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `login_user`
 --
 ALTER TABLE `login_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=92;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=95;
 
 --
 -- AUTO_INCREMENT for table `orders_db`
 --
 ALTER TABLE `orders_db`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=145;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=163;
 
 --
 -- AUTO_INCREMENT for table `room_pirce`
 --
 ALTER TABLE `room_pirce`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `villa_about`
